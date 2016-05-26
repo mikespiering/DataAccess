161f519dea6d2735eb080b3db4237a654b3def15
Michael Spiering <mspiering@pivotal.io>
2016-05-26 11:37:44 -0400

    init commit

---
+++ src/main/java/hello/Location.java
@@ -0,0 +1,34 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String zipCode;
+    private String data;
+
+    protected Location() {}
+
+    
+    public Location(String zipCode, String data) {
+        this.zipCode = zipCode;
+        this.data = data;
+    }
+    
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Information[id=%d, zipCode='%s', data='%s']",
+                id, zipCode, data);
+    }
+
+}
+
